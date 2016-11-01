4b4d5591f8dc6708cc95699fffd64bcdd49dad78
Jiri Hon <ihon@fit.vutbr.cz>
2016-11-01 11:09:32 +0100

    Better missing staff fill.

--- jchs_part.py
+++ jchs_part.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from __future__ import division
 import cv2
 import numpy as np
@@ -10,35 +11,39 @@ import re
 import glob
 
 songs = [
-    {'dir': '01_Predehra', 'slug': '01', 'title': 'Předehra'},
+    # {'dir': '01_Predehra', 'slug': '01', 'title': 'Předehra'},
     {'dir': '02_Jak_ze_sna_procitam', 'slug': '02', 'title': 'Jak ze sna procitám'},
-    {'dir': '03a_Proc_ten_shon', 'slug': '03a', 'title': 'Proč ten shon'},
-    {'dir': '03b_Divna_mystifikace', 'slug': '03b', 'title': 'Divná mystifikace'},
-    {'dir': '04_Vse_je_tak_jak_ma_byt', 'slug': '04', 'title': 'Vše je tak, jak má být'},
-    {'dir': '05_Zemrit_by_mel', 'slug': '05', 'title': 'Zemřít by měl'},
-    {'dir': '06_Hosanna', 'slug': '06', 'title': 'Hosanna'},
-    {'dir': '07_Simon_Zelotes_Ubohy_Jeruzalem', 'slug': '07', 'title': 'Šimon Zélotes - Ubohý Jeruzalém'},
-    {'dir': '08_Pilatuv_sen', 'slug': '08', 'title': 'Pilátův sen'},
-    {'dir': '09_V_chramu', 'slug': '09', 'title': 'V chrámu'},
-    {'dir': '10_Vse_je_tak_jak_ma_byt_II_Co_na_tom_je_tak_zleho', 'slug': '10', 'title': 'Vše je tak, jak má být II - Co na tom je tak zlého'},
-    {'dir': '11_Zavrzen_na_veky_vekuv_Penize_zkropene_krvi', 'slug': '11', 'title': 'Zavržen na věky věkův - Peníze zkropené krví'}
+    # {'dir': '03a_Proc_ten_shon', 'slug': '03a', 'title': 'Proč ten shon'},
+    # {'dir': '03b_Divna_mystifikace', 'slug': '03b', 'title': 'Divná mystifikace'},
+    # {'dir': '04_Vse_je_tak_jak_ma_byt', 'slug': '04', 'title': 'Vše je tak, jak má být'},
+    # {'dir': '05_Zemrit_by_mel', 'slug': '05', 'title': 'Zemřít by měl'},
+    # {'dir': '06_Hosanna', 'slug': '06', 'title': 'Hosanna'},
+    # {'dir': '07_Simon_Zelotes_Ubohy_Jeruzalem', 'slug': '07', 'title': 'Šimon Zélotes - Ubohý Jeruzalém'},
+    # {'dir': '08_Pilatuv_sen', 'slug': '08', 'title': 'Pilátův sen'},
+    # {'dir': '09_V_chramu', 'slug': '09', 'title': 'V chrámu'},
+    # {'dir': '10_Vse_je_tak_jak_ma_byt_II_Co_na_tom_je_tak_zleho', 'slug': '10', 'title': 'Vše je tak, jak má být II - Co na tom je tak zlého'},
+    # {'dir': '11_Zavrzen_na_veky_vekuv_Penize_zkropene_krvi', 'slug': '11', 'title': 'Zavržen na věky věkův - Peníze zkropené krví'}
 ]
 instruments = [
-    {'slug': 'Tbn', 'regex': re.compile('(T|\'I\')(rombone|[bh][nu])')},
-    {'slug': 'Flt', 'regex': re.compile('(Flute|F[l1LI\]\[\}\{])')},
-    {'slug': 'Clr', 'regex': re.compile('.*(Clarinet|C[l1LI\]\[\}\{])')},
-    {'slug': 'Tpt', 'regex': re.compile('.*(Trumpet|Tp[tlL]|[B8].* m.)')}
+    # {'slug': 'Tbn', 'regex': re.compile('(T|\'I\')(rombone|[bh][nu])')},
+    # {'slug': 'Flt', 'regex': re.compile('(Flute|F[l1LI\]\[\}\{])')},
+    # {'slug': 'Clr', 'regex': re.compile('.*(Clarinet|C[l1LI\]\[\}\{]|Bassoon|Bsn|Tenor|Sx)')},
+    # {'slug': 'Tpt', 'regex': re.compile('.*(Trumpet|Tp[tlL]|[B8].* m.)')},
+    {'slug': 'Bsn', 'regex': re.compile('(Bassoon|Bsn)')},
+    # {'slug': 'KbdStr', 'regex': re.compile('.*(Voice|Choir|Keyboard|K[bh]d)'), 'top': -200, 'bottom_end': -100,
+    #  'exclude': {'regex': re.compile('(.*(Guitar|G[tlL]r|Dr)|^Bass$)'), 'top': -80, 'bottom': +80}}
 ]
 score_x_shift = -30
 label_x_offset = -150
 first_label_extra_x_offset = -100
 label_first_width = 270
 label_other_width = 170
-page_height_limit = 2590
+page_height_limit = 2580#2590
 score_width = 4250
 top_y_offset = -120
 bottom_y_offset = +100
 white_color = (255, 255, 255)
+page_fill_threshold = 200
 
 tex_header = '''
 \\documentclass[]{article}
@@ -63,7 +68,7 @@ tex_footer = '''
 tex_image = '''
 \\begin{figure}
 \\centering
-\\includegraphics[width=1\\linewidth]{%s}
+\\includegraphics[width=1\\linewidth,height=1.05\\textheight,keepaspectratio]{%s}
 \\end{figure}
 '''
 
@@ -71,7 +76,7 @@ tex_image = '''
 def preprocess(files):
     for path in files:
         filename, file_extension = os.path.splitext(path)
-        print 'file:', filename, file_extension
+        print('file:', filename, file_extension)
 
         img = cv2.imread(path)
         height, width, channels = img.shape
@@ -95,7 +100,7 @@ def preprocess(files):
                 min_sum = sum
                 opt_angle = angle
             angle += 0.1
-        print 'opt_angle:', opt_angle
+        print('opt_angle:', opt_angle)
 
         rot_m = cv2.getRotationMatrix2D((width / 2, height / 2), opt_angle, 1)
         img_r = cv2.warpAffine(
@@ -112,7 +117,7 @@ def preprocess(files):
                 break
             score_x += 1
 
-        print 'score_x:', score_x
+        print('score_x:', score_x)
 
         if filename.split('/')[-1] == 'page-000':
             label_width = label_first_width
@@ -130,21 +135,22 @@ def preprocess(files):
 def ocr(files):
     for path in files:
         filename, file_extension = os.path.splitext(path)
-        print 'file:', filename, file_extension
+        print('file:', filename, file_extension)
         os.system('tesseract %s.png %s -l eng hocr' % (filename, filename))
 
 
-def extract(files, instr, regex):
-    staff_per_page_sum = 0
-    staff_per_page_cnt = 0
-    staff_height_sum = 0
-    page_height_sum = 0
-    page_cnt = 0
-    staff_cnt = 0
+def extract(files, instr):
+    # staff_per_page_sum = 0
+    # staff_per_page_cnt = 0
+    # staff_height_sum = 0
+    # page_height_sum = 0
+    # page_cnt = 0
+    # staff_cnt = 0
+    # filename = ''
 
     for path in files:
-        filename, file_extension = os.path.splitext(path)
-        print 'file:', filename, file_extension
+        filename, _ = os.path.splitext(path)
+        print('file:', filename)
 
         tree = etree.parse('%s-instr.hocr' % filename)
         root = tree.getroot()
@@ -154,19 +160,26 @@ def extract(files, instr, regex):
         right_x = 0
         top_y = 0
         bottom_y = 0
+        excl_top_y = 0
+        excl_bottom_y = 0
         label_x_sum = 0
         label_x_cnt = 0
 
         for par in root.findall("./x:body/x:div/x:div/x:p/x:span[@class='ocr_line']", ns):
             text = "".join(par.itertext()).strip()
             coords = par.attrib['title'].split(';')[0].split(' ')
-            if re.match(regex, text):
-                print text, coords
+            if re.match(instr['regex'], text):
+                print(text, coords)
                 if top_y == 0:
                     top_y = int(coords[2])
                 bottom_y = int(coords[4])
                 label_x_sum += int(coords[3])
                 label_x_cnt += 1
+            if 'exclude' in instr and re.match(instr['exclude']['regex'], text):
+                print(text, coords)
+                if excl_top_y == 0:
+                    excl_top_y = int(coords[2])
+                excl_bottom_y = int(coords[4])
 
         label_x = int(label_x_sum / label_x_cnt)
 
@@ -177,72 +190,137 @@ def extract(files, instr, regex):
             left_x += first_label_extra_x_offset
             right_x += first_label_extra_x_offset
 
-        top_y += top_y_offset
-        bottom_y += bottom_y_offset
+        if 'top' in instr:
+            top_y += instr['top']
+        else:
+            top_y += top_y_offset
+
+        if 'bottom' in instr:
+            bottom_y += instr['bottom_y']
+        elif 'bottom_end' in instr:
+            bottom_y = height + instr['bottom_end']
+            print('bottom_end', bottom_y)
+        else:
+            bottom_y += bottom_y_offset
         left_x += label_x + label_x_offset
         right_x += left_x + score_width
 
         left_x = max(0, left_x)
         right_x = min(width, right_x)
 
-        cv2.imwrite('%s-%s.png' % (filename, instr), img[top_y:bottom_y, left_x:right_x])
-
-        height = bottom_y - top_y
-        staff_height_sum += height
-        staff_cnt += 1
-
-        if page_height_sum + height > page_height_limit:
-            print page_height_sum, height, page_height_sum + height
-            page_height_sum = height
-            staff_per_page_sum += staff_per_page_cnt
-            staff_per_page_cnt = 1
-            page_cnt += 1
+        if 'exclude' in instr:
+            excl_top_y += instr['exclude']['top']
+            excl_bottom_y += instr['exclude']['bottom']
+            img_top = img[top_y:excl_top_y, left_x:right_x]
+            img_bottom = img[excl_bottom_y:bottom_y, left_x:right_x]
+            img = np.concatenate((img_top, img_bottom), axis=0)
         else:
-            staff_per_page_cnt += 1
-            page_height_sum += height
+            img = img[top_y:bottom_y, left_x:right_x]
 
-    staff_height = int(staff_height_sum / staff_cnt)
+        cv2.imwrite('%s-%s.png' % (filename, instr['slug']), img)
 
-    if page_cnt > 0:
-        staff_per_page = int(staff_per_page_sum / page_cnt)
-    else:
-        staff_per_page = staff_per_page_cnt
+    #     height = bottom_y - top_y
+    #     staff_height_sum += height
+    #     staff_cnt += 1
+    #
+    #     if page_height_sum + height > page_height_limit:
+    #         print(page_height_sum, height, page_height_sum + height)
+    #         page_height_sum = height
+    #         staff_per_page_sum += staff_per_page_cnt
+    #         staff_per_page_cnt = 1
+    #         page_cnt += 1
+    #     else:
+    #         staff_per_page_cnt += 1
+    #         page_height_sum += height
+    #
+    # staff_height = int(staff_height_sum / staff_cnt)
+    #
+    # if page_cnt > 0:
+    #     staff_per_page = int(staff_per_page_sum / page_cnt)
+    # else:
+    #     staff_per_page = staff_per_page_cnt
+    #
+    # missing_staff_cnt = staff_per_page - staff_per_page_cnt
+    # print(staff_per_page, staff_per_page_cnt, page_cnt, staff_height)
+    #
+    # img_empty = np.zeros((staff_height, score_width, 3), np.uint8)
+    # img_empty[:] = white_color
+    #
+    # filename_prefix = ''.join(filename.split('-')[:-1])
+    # os.system('rm %s-9*-%s.png' % (filename_prefix, instr['slug']))
+    #
+    # while missing_staff_cnt > 0:
+    #     cv2.imwrite(
+    #         '%s-9%02d-%s.png' % (filename_prefix, missing_staff_cnt, instr['slug']),
+    #         img_empty)
+    #     missing_staff_cnt -= 1
 
-    missing_staff_cnt = staff_per_page - staff_per_page_cnt
-    print staff_per_page, staff_per_page_cnt, page_cnt, staff_height
+def run_tex(song, instr):
+    files = glob.glob('%s/page-*-%s.png' % (song['dir'], instr['slug']))
+    files.sort()
 
-    img_empty = np.zeros((staff_height, score_width, 3), 'uint8')
-    img_empty[:] = white_color
-
-    filename_prefix = ''.join(filename.split('-')[:-1])
-    os.system('rm %s-9*-%s.png' % (filename_prefix, instr))
-
-    while missing_staff_cnt > 0:
-        cv2.imwrite(
-            '%s-9%02d-%s.png' % (filename_prefix, missing_staff_cnt, instr),
-            img_empty)
-        missing_staff_cnt -= 1
-
-
-def export(files, song_slug, song_title, instr):
-    f = open('tex/%s_%s.tex' % (song_slug, instr), 'w')
-    f.write(tex_header % song_title)
+    f = open('tex/%s_%s.tex' % (song['slug'], instr['slug']), 'w')
+    f.write(tex_header % song['title'])
     for path in files:
         f.write(tex_image % ('../%s' % path))
-
     f.write(tex_footer)
     f.close()
 
-    outname = '%s_%s' % (song_slug, instr)
+    outname = '%s_%s' % (song['slug'], instr['slug'])
     os.system("cd tex; pdflatex %s.tex" % outname)
-    os.system('mkdir -p %s' % instr)
-    os.system('mv tex/%s.pdf %s/' % (outname, instr))
-    os.system('rm tex/%s.log tex/%s.aux' % (outname, outname))
+    os.system('mkdir -p %s' % instr['slug'])
+    os.system('mv tex/%s.pdf %s/' % (outname, instr['slug']))
+
+
+def export(song, instr):
+    os.system('rm %s/page-9*-%s.png' % (song['dir'], instr['slug']))
+    # Run first pdflatex run
+    run_tex(song, instr)
+    # Analyse distribution of staffs on pages
+    f = open('tex/%s_%s.log' % (song['slug'], instr['slug']), 'r')
+    log = ''.join(map(str.strip, f.readlines()))
+    pages = re.findall('\[\d+[^\]]*\]', log)
+    staff_heights = []
+    for page in pages:
+        staffs = re.findall('<../([^>]+.png)[^>]*>', page)
+        heights = []
+        staff_heights.append(heights)
+        for staff in staffs:
+            img = cv2.imread(staff)
+            heights.append(img.shape[0])
+
+    print(staff_heights)
+
+    missing_staff_cnt = 0
+    missing_staff_height = 0
+
+    if len(staff_heights) > 1:
+        mean_page_height = int(sum(map(sum, staff_heights[:-1])) / len(staff_heights[:-1]))
+        mean_page_staff_cnt = int(sum(map(len, staff_heights[:-1])) / len(staff_heights[:-1]))
+        last_page_height = sum(staff_heights[-1])
+        last_page_staff_cnt = len(staff_heights[-1])
+
+        if mean_page_height - last_page_height > page_fill_threshold:
+            missing_staff_cnt = max(1, mean_page_staff_cnt - last_page_staff_cnt)
+            missing_staff_height = int((mean_page_height - last_page_height) / missing_staff_cnt)
+
+    print(missing_staff_cnt, missing_staff_height)
+
+    img_empty = np.zeros((missing_staff_height, score_width, 3), np.uint8)
+    img_empty[:] = white_color
+
+    while missing_staff_cnt > 0:
+        cv2.imwrite('%s/page-9%02d-%s.png' % (song['dir'], missing_staff_cnt, instr['slug']),
+            img_empty)
+        missing_staff_cnt -= 1
+
+    # Run second pdflatex run
+    run_tex(song, instr)
 
 
 def unpdf():
     for song in songs:
-        print 'song:', song['dir']
+        print('song:', song['dir'])
         os.system('mkdir %s' % song['dir'])
         os.system('pdfimages %s.pdf %s/page' % (song['dir'], song['dir']))
 
@@ -272,17 +350,17 @@ elif action == 'ocr':
     ocr(files)
 elif action == 'extract':
     for song in songs:
-        print 'song:', song['dir']
+        print('song:', song['dir'])
         files = glob.glob('%s/page-*.p*m' % song['dir'])
         files.sort()
         for instr in instruments:
-            extract(files, instr['slug'], instr['regex'])
+            extract(files, instr)
 elif action == 'export':
     for song in songs:
-        print 'song:', song['dir']
+        print('song:', song['dir'])
         for instr in instruments:
-            files = glob.glob('%s/page-*-%s.png' % (song['dir'], instr['slug']))
-            files.sort()
-            export(files, song['slug'], song['title'], instr['slug'])
+            export(song, instr)
 elif action == 'unpdf':
     unpdf()
+elif action == 'pagelimit':
+    pagelimit()
